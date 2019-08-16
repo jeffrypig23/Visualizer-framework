c7f68440bea8849b13baa99cdfb026c229fe5ca1
Spud <jeffrypig23@gmail.com>
2019-08-16 14:00:16 -0700

    Added the RelativeNode interface

---
+++ src/UI/RelativeNode.java
@@ -0,0 +1,20 @@
+package UI;
+
+/**
+ * An interface for all the nodes that need to have their respective positions relative based on the windows size.
+ * <p>
+ * https://stackoverflow.com/questions/6270132/create-a-custom-event-in-java
+ *
+ * @author Spud
+ */
+public interface RelativeNode {
+
+	/**
+	 * Sets the relative position of the node in the window based off of the provided width and height.
+	 *
+	 * @param width  The current window width.
+	 * @param height The current window height.
+	 */
+	public void updatePosition(double width, double height);
+
+}
