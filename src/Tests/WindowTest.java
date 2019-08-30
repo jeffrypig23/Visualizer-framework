f2d2176c8b15faa8fb27615e9b790a7dc49720f2
Spud <jeffrypig23@gmail.com>
2019-08-30 15:07:12 -0800

    Updated the unit test to wait for the window to be displayed before running

--- src/Tests/WindowTest.java
+++ src/Tests/WindowTest.java
@@ -3,12 +3,16 @@ import UI.Objects.Bar;
 import UI.Objects.LoadingBar;
 import UI.Objects.Monstercat;
 import UI.Window;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import name_me.Debugger;
+import name_me.GenreColors;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class WindowTest extends Application {
 
@@ -54,48 +58,80 @@ public class WindowTest extends Application {
 		Debugger.DEBUG = false;
 
 		// First, create the window
-		UI.Window window = new Window(primaryStage);
-		window.stage.show();
-		window.updateOpacity(0.5d);
+		final UI.Window window = new Window(primaryStage);
+		//window.updateOpacity(0.5d);
 
+		// Add a listener for on shown to then run tests based on parameters
+		window.stage.setOnShown((event) -> {
 
-		// Now check for arguments (tests)
-		for (String parameter : getParameters().getRaw()) {
-			switch (parameter) {
-				case "albumart_test":  // Show the album art object
-					window.addToWindow(new AlbumArt());
-					break;
-				case "bar_test":  // Show the bar object
-					window.addToWindow(new Bar());
-					//window.addToWindow(new AlbumArt());
-					break;
-				case "bars_test":  // Show multiple bars
-					window.addToWindow(new AlbumArt());
-					ArrayList<Bar> bars = new ArrayList<Bar>();
-					for (int i = 0; i < 63; i++) {
-						Bar bar = new Bar(i);
-						window.addToWindow(bar);
-						bars.add(bar);
+			// Now check for parameters (tests)
+			for (String parameter : getParameters().getRaw()) {
+				try {
+					switch (parameter) {
+						case "albumart_test":
+							// Show the album art object, and run its various functions
+							AlbumArt albumArt = new AlbumArt();
+							window.addToWindow(albumArt);
+
+							// Abuse the PauseTransition object for changing colors
+							PauseTransition transition = new PauseTransition(Duration.millis(500));
+							albumArt.setColor(GenreColors.values()[0]);
+							transition.setOnFinished((finished -> {
+								try {
+									// Parse the current color to find the current index
+									int index = Objects.requireNonNull(GenreColors.getGenre(albumArt.getColor())).ordinal();
+									if (index < GenreColors.values().length - 1) {
+										albumArt.setColor(GenreColors.values()[index + 1]);
+
+										// Check for errors, then play
+										transition.play();
+
+									} else {
+										System.exit(0);
+									}
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}));
+							transition.play();
+							break;
+						case "bar_test":  // Show the bar object
+							window.addToWindow(new Bar());
+							//window.addToWindow(new AlbumArt());
+							break;
+						case "bars_test":  // Show multiple bars
+							window.addToWindow(new AlbumArt());
+							ArrayList<Bar> bars = new ArrayList<Bar>();
+							for (int i = 0; i < 63; i++) {
+								Bar bar = new Bar(i);
+								window.addToWindow(bar);
+								bars.add(bar);
+							}
+							bars.get(12).setBarHeight(1);
+							break;
+						case "loadingbar_test":
+							window.addToWindow(new AlbumArt());
+							window.addToWindow(new LoadingBar());
+							break;
+						case "loadingbar_animation_test":
+							LoadingBar loadingBar = new LoadingBar();
+							window.addToWindow(loadingBar);
+							loadingBar.playAnimation();
+							break;
+						case "Monstercat_test":
+							window.addToWindow(new AlbumArt());
+							window.addToWindow(new Monstercat());
+							break;
+						default:
+							System.out.println("Unrecognized parameter: " + parameter);
+							break;
 					}
-					bars.get(12).setBarHeight(1);
-					break;
-				case "loadingbar_test":
-					window.addToWindow(new AlbumArt());
-					window.addToWindow(new LoadingBar());
-					break;
-				case "loadingbar_animation_test":
-					LoadingBar loadingBar = new LoadingBar();
-					window.addToWindow(loadingBar);
-					loadingBar.playAnimation();
-					break;
-				case "Monstercat_test":
-					window.addToWindow(new AlbumArt());
-					window.addToWindow(new Monstercat());
-					break;
-				default:
-					System.out.println("Unrecognized parameter: " + parameter);
-					break;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		}
+		});
+
+		window.stage.show();
 	}
 }
